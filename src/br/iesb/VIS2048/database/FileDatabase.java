031d6834b35c9c426e3644237a137de2a3b5191f
VictorAug <victor@loopequipamentos.com.br>
2014-12-07 21:43:44 -0200

    Update FileDatabase.java

--- src/br/iesb/VIS2048/database/FileDatabase.java
+++ src/br/iesb/VIS2048/database/FileDatabase.java
@@ -1,51 +1,85 @@
 package br.iesb.VIS2048.database;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class FileDatabase {
+public class FileDataBase {
+	
 	private String appPathString = "./data";
 	private String DBPathString = appPathString+"/DB";
 	private String DBFile = DBPathString + "/DB1.txt";
 	
-	private boolean createDirectory(String path){
-		if(Files.exists(Paths.get(path))){
-			System.out.println("Diretório já existe!");
+	public FileDataBase() {
+		if(createDirectory(appPathString)) System.out.println("Podemos continuar");
+		if(createDirectory(DBPathString)) System.out.println("Podemos continuar");
+		if(createFile(DBFile)) System.out.println("Podemos continuar");
+//		int vetSize = 20;
+
+//		double vet[] = fillRandomVector(new double[vetSize], vetSize); 
+//		System.out.println(System.currentTimeMillis());
+//		//for(double i = 0; i<1000; i++)
+//		logVector(System.currentTimeMillis(), vet);
+//		//readFromFile();
+//		//System.out.println(System.currentTimeMillis() / 1000L);
+//		
+//		try(BufferedReader br = new BufferedReader(new FileReader(DBFile))) {
+//			String[] dados = new String[vetSize];
+//			double lineNumber = 1;
+//			String line;
+//			Pattern pattern = Pattern.compile("^\\d+");
+//			Matcher matcher;
+//			while((line = br.readLine()) != null) {
+//				matcher = pattern.matcher(line);
+//				while (matcher.find()) {
+//					Date date = new Date( Long.parseLong(matcher.group(), 10) );
+//					SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss aa",Locale.getDefault());
+//					System.out.println("Linha: "+lineNumber + " - Data: " + format.format(date));
+//				}
+//				Pattern replace = Pattern.compile("^\\d+,");
+//				Matcher matcher2 = replace.matcher(line);
+//				System.out.println("Dados: " + matcher2.replaceAll(""));
+//				
+//				dados = matcher2.replaceAll("").split(",");
+//				for(String a : dados)
+//					System.out.println(Double.parseDouble(a));
+//				System.out.println();
+//				lineNumber++;
+//			}
+//			// line is not visible here.
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+	}
+	
+	private boolean createDirectory(String path) {
+		if(Files.exists(Paths.get(path))) {
+			System.out.println("DiretÃ³rio jÃ¡ existe!");
 			return true;
-		}else{
-			System.out.println("Diretório já existe!");
+		} else {
 			File dir = new File(path); 
-			if(dir.mkdir()){
-				System.out.println("Diretório criado com sucesso");
+			if(dir.mkdir()) {
+				System.out.println("DiretÃ³rio criado com sucesso");
 				return true;
-			}else{
-				System.out.println("Diretório não pôde ser criado");
+			} else {
+				System.out.println("DiretÃ³rio nÃ£o pÃ´de ser criado");
 				return false;
 			}
 		}
 	}
 	
-	private boolean createFile(String path){
+	private boolean createFile(String path) {
 		File f = new File(path);
-		if(f.isFile()){ 
-			System.out.println("Arquivo já existe!");
+		if(f.isFile()) { 
+			System.out.println("Arquivo jÃ¡ existe!");
 			return true;
-		}
-		else{
-			System.out.println("Arquivo não existe");
+		} else {
+			System.out.println("Arquivo nÃ£o existe");
 			try {
-				if(f.createNewFile()){
+				if(f.createNewFile()) { 
 					System.out.println("Arquivo criado");
 					return true;
 				}
@@ -79,25 +113,25 @@ public class FileDatabase {
 //		return false;
 //	}
 	
-	private double[] fillRandomVector(double[] vet, int size){
+	public double[] fillRandomVector(double[] vet, int size) {
 		vet = new double[size];
-		for (int i=0;i<size;i++)
+		for (int i = 0; i < size; i++)
 			vet[i] = Math.random() * 1000;
 		return vet;
 	}
 	
-	private boolean logVector(long timestamp, double[] vet){
+	public boolean logVector(long timestamp, double[] vet) {
 		String text = String.valueOf(timestamp);
-		for (int i=0;i<10;i++)
+		for (int i = 0; i < 10; i++)
 			text = text + ',' + String.valueOf(vet[i]);
 		writeToFile(text);
 		return true;
 	}
 	
-	private boolean writeToFile(String text){
+	private boolean writeToFile(String text) {
 		try {
 			File f = new File(DBFile);
-			FileWriter fw = new FileWriter(f,true);
+			FileWriter fw = new FileWriter(f, true);
 			PrintWriter output = new PrintWriter(fw);
 			output.println(text);
 			output.close();
@@ -108,49 +142,5 @@ public class FileDatabase {
 		} 
 		return false;
 	}
-	
-	FileDatabase(){
-		if(createDirectory(appPathString)) System.out.println("Podemos continuar");
-		if(createDirectory(DBPathString)) System.out.println("Podemos continuar");
-		if(createFile(DBFile)) System.out.println("Podemos continuar");
-		int vetSize = 10;
-		double vet[] = fillRandomVector(new double[vetSize], vetSize); 
-		System.out.println(System.currentTimeMillis());
-		//for(double i = 0; i<1000; i++)
-			logVector(System.currentTimeMillis(), vet);
-		//readFromFile();
-		//System.out.println(System.currentTimeMillis() / 1000L);
-		
-		try(BufferedReader br = new BufferedReader(new FileReader(DBFile))) {
-			String[] dados = new String[vetSize];
-			double lineNumber = 1;
-			String line;
-			Pattern pattern = Pattern.compile("^\\d+");
-		    Matcher matcher;
-		    while((line = br.readLine()) != null) {
-		    	matcher = pattern.matcher(line);
-		    	while (matcher.find()) {
-		    		Date date = new Date( Long.parseLong(matcher.group(), 10) );
-		    		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss aa",Locale.getDefault());
-		    		System.out.println("Linha: "+lineNumber + " - Data: " + format.format(date));
-		    	}
-		    	Pattern replace = Pattern.compile("^\\d+,");
-		        Matcher matcher2 = replace.matcher(line);
-		        System.out.println("Dados: " + matcher2.replaceAll(""));
-		        
-		        dados = matcher2.replaceAll("").split(",");
-		        for(String a : dados)
-		        	System.out.println(Double.parseDouble(a));
-		    	System.out.println();
-		    	lineNumber++;
-		    }
-		    // line is not visible here.
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-
 	
 }
