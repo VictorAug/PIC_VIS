c58210331549d9a4a68ab7564a55b5370f7712e3
VictorAug <victor@loopequipamentos.com.br>
2015-03-11 17:23:17 -0300

    Correção de NullPointerException.

--- src/br/iesb/VIS2048/database/DBHandler.java
+++ src/br/iesb/VIS2048/database/DBHandler.java
@@ -26,22 +26,22 @@ public class DBHandler {
 
     /** Constante appPathString. */
     private final static String appPathString = "./Vis/";
-    
+
     /** Constante DBPathString. */
     private final static String DBPathString = appPathString + "DB/";
-    
+
     /** Atributo DB file config. */
     private static String DBFileConfig = DBPathString + "config/";
-    
+
     /** Atributo DB file collection. */
     private static String DBFileCollection = DBPathString + "collection/";
-    
+
     /** Atributo collection. */
     private ArrayList<String> collection = new ArrayList<String>();
-    
+
     /** Atributo main db. */
     private String mainDB;
-    
+
     /** Atributo DB chart. */
     private DBChartCollection DBChart;
 
@@ -62,7 +62,7 @@ public class DBHandler {
 
 	for (int i = 1; i < collection.size() + 2; i++) {
 	    if (!collection.contains(("solucao" + i + ".vis").toLowerCase())) {
-		mainDB.concat("solucao" + i + ".vis");
+		mainDB += "solucao" + i + ".vis";
 		break;
 	    }
 	}
@@ -91,7 +91,8 @@ public class DBHandler {
     /**
      * Creates the dir.
      *
-     * @param path the path
+     * @param path
+     *            the path
      */
     private static void createDir(String path) {
 	if (!Files.exists(Paths.get(path))) {
@@ -111,7 +112,8 @@ public class DBHandler {
     /**
      * Retorna files list.
      *
-     * @param directoryName the directory name
+     * @param directoryName
+     *            the directory name
      * @return files list
      */
     public static File[] getFilesList(String directoryName) {
@@ -121,7 +123,8 @@ public class DBHandler {
     /**
      * List files and folders.
      *
-     * @param directoryName the directory name
+     * @param directoryName
+     *            the directory name
      */
     public void listFilesAndFolders(String directoryName) {
 	File directory = new File(directoryName);
@@ -134,27 +137,25 @@ public class DBHandler {
     /**
      * List files.
      *
-     * @param directoryName the directory name
+     * @param directoryName
+     *            the directory name
      */
     public void listFiles(String directoryName) {
 	File directory = new File(directoryName);
 	File[] fList = directory.listFiles();
-	Arrays.asList(fList).stream()
-        	.filter(file -> file.isFile())
-        	.forEach(file -> System.out.println(file.getName()));
+	Arrays.asList(fList).stream().filter(file -> file.isFile()).forEach(file -> System.out.println(file.getName()));
     }
 
     /**
      * List folders.
      *
-     * @param directoryName the directory name
+     * @param directoryName
+     *            the directory name
      */
     public void listFolders(String directoryName) {
 	File directory = new File(directoryName);
 	File[] fList = directory.listFiles();
-	Arrays.asList(fList).stream()
-        	.filter(file -> file.isFile())
-        	.forEach(file -> System.out.println(file.getName()));
+	Arrays.asList(fList).stream().filter(file -> file.isFile()).forEach(file -> System.out.println(file.getName()));
     }
 
     // public double[] fillRandomVector(double[] vet, int size) {
@@ -167,8 +168,10 @@ public class DBHandler {
     /**
      * Save g zip object.
      *
-     * @param vlo the vlo
-     * @param fileName the file name
+     * @param vlo
+     *            the vlo
+     * @param fileName
+     *            the file name
      * @return true, se bem-sucedido
      */
     public static boolean saveGZipObject(Object vlo, String fileName) {
@@ -194,7 +197,8 @@ public class DBHandler {
     /**
      * Load g zip object.
      *
-     * @param fileName the file name
+     * @param fileName
+     *            the file name
      * @return object
      */
     public static Object loadGZipObject(String fileName) {
@@ -221,11 +225,16 @@ public class DBHandler {
     /**
      * Log vector.
      *
-     * @param leitura the leitura
-     * @param name the name
-     * @param description the description
-     * @param numberOfSamples the number of samples
-     * @param timestamp the timestamp
+     * @param leitura
+     *            the leitura
+     * @param name
+     *            the name
+     * @param description
+     *            the description
+     * @param numberOfSamples
+     *            the number of samples
+     * @param timestamp
+     *            the timestamp
      * @return true, se bem-sucedido
      */
     public static boolean logVector(Double[] leitura, String name, String description, int numberOfSamples, long timestamp) {
@@ -250,7 +259,8 @@ public class DBHandler {
     /**
      * Write to file.
      *
-     * @param text the text
+     * @param text
+     *            the text
      * @return true, se bem-sucedido
      */
     private static boolean writeToFile(String text) {
@@ -273,9 +283,11 @@ public class DBHandler {
     /**
      * Compress.
      *
-     * @param str the str
+     * @param str
+     *            the str
      * @return string
-     * @throws IOException Sinaliza que uma I/O exception ocorreu.
+     * @throws IOException
+     *             Sinaliza que uma I/O exception ocorreu.
      */
     public static String compress(String str) throws IOException {
 	if (str == null || str.length() == 0) {
@@ -294,9 +306,11 @@ public class DBHandler {
     /**
      * Decompress.
      *
-     * @param str the str
+     * @param str
+     *            the str
      * @return string
-     * @throws IOException Sinaliza que uma I/O exception ocorreu.
+     * @throws IOException
+     *             Sinaliza que uma I/O exception ocorreu.
      */
     public static String decompress(String str) throws IOException {
 	if (str == null || str.length() == 0) {
@@ -335,7 +349,8 @@ public class DBHandler {
     /**
      * Atribui o valor main db.
      *
-     * @param mainDB novo main db
+     * @param mainDB
+     *            novo main db
      */
     public void setMainDB(String mainDB) {
 	if (mainDB == null) {
@@ -357,7 +372,8 @@ public class DBHandler {
     /**
      * Atribui o valor DB file collection.
      *
-     * @param dBFileCollection novo DB file collection
+     * @param dBFileCollection
+     *            novo DB file collection
      */
     public static void setDBFileCollection(String dBFileCollection) {
 	if (dBFileCollection == null) {
@@ -370,8 +386,10 @@ public class DBHandler {
     /**
      * Save chart collection.
      *
-     * @param chartCollection the chart collection
-     * @param mainDBFileName the main db file name
+     * @param chartCollection
+     *            the chart collection
+     * @param mainDBFileName
+     *            the main db file name
      */
     public static void saveChartCollection(DBChartCollection chartCollection, String mainDBFileName) {
 	System.out.println(chartCollection.count());
